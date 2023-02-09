24da17e92384399c2bbbfc393943a3a0b0863f23
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2023-02-09 15:37:27 +0100

    add integration test in Bill.js but test is bugging

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -3,12 +3,14 @@
  */
 
 import { screen, waitFor } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
 import BillsUI from '../views/BillsUI.js';
 import { bills } from '../fixtures/bills.js';
-import { ROUTES_PATH } from '../constants/routes.js';
+import { ROUTES, ROUTES_PATH } from '../constants/routes.js';
 import { localStorageMock } from '../__mocks__/localStorage.js';
 
 import router from '../app/Router.js';
+import Bills from '../containers/Bills.js';
 
 describe('Given I am connected as an employee', () => {
   describe('When I am on Bills Page', () => {
@@ -30,6 +32,7 @@ describe('Given I am connected as an employee', () => {
       await waitFor(() => screen.getByTestId('icon-window'));
       const windowIcon = screen.getByTestId('icon-window');
       //to-do write expect expression
+      expect(windowIcon.getAttribute('class')).toContain('active-icon');
     });
     test('Then bills should be ordered from earliest to latest', () => {
       document.body.innerHTML = BillsUI({ data: bills });
@@ -44,3 +47,44 @@ describe('Given I am connected as an employee', () => {
     });
   });
 });
+
+describe('Given I am connected as an employee and I am on bills page', () => {
+  describe('When I click on the icon eye', () => {
+    test('A modal should open', () => {
+      // start connexion simulation
+      Object.defineProperty(window, 'localStorage', {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        'user',
+        JSON.stringify({
+          type: 'Employee',
+        })
+      );
+      // end connexion simulation
+      //start DOM simulation
+      document.body.innerHTML = BillsUI({ data: bills });
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      const store = null;
+      const bill = new Bills({
+        document,
+        onNavigate,
+        store,
+        localStorage: window.localStorage,
+      });
+      //end DOM simulation
+      //start eventlistener simulation
+      const iconEye = screen.getAllByTestId('icon-eye');
+      const icon1 = iconEye[0];
+      const handleClickIconEye = jest.fn(bill.handleClickIconEye(icon1));
+      icon1.addEventListener('click', handleClickIconEye);
+      //end eventlistener simulation
+      //start user click simulation
+      userEvent.click(icon1);
+      expect(handleClickIconEye).toHaveBeenCalled();
+      //end user click simulation
+    }); //end test
+  }); //end describe
+}); //end describe
